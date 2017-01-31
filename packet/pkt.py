540df2f424fdc08195a9669457263ca1d930272e
Jorge Mora <mora@netapp.com>
2017-01-31 16:34:16 -0700

    PKT: do not display the data attribute for NFS by default

--- packet/pkt.py
+++ packet/pkt.py
@@ -131,7 +131,7 @@ class Pkt(BaseObj):
                     else:
                         sps = " " * (_maxlen - len(key))
                         out += "    %s:%s %s\n" % (key.upper(), sps, str(value))
-                        if index == lastkey and hasattr(value, "data") and value.data is not None:
+                        if index == lastkey and hasattr(value, "data") and value.data is not None and key != "nfs":
                             sps = " " * (_maxlen - 4)
                             out += "    DATA:%s 0x%s\n" % (sps, value.data.encode("hex"))
                 index += 1
