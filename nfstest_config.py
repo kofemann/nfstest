9ae9c4b1b1e03d61ad60c8f23b7dd64864428505
Jorge Mora <mora@netapp.com>
2018-02-28 08:55:38 -0700

    CONFIG: change version to 2.1.7

--- nfstest_config.py
+++ nfstest_config.py
@@ -14,7 +14,7 @@
 import os
 
 NFSTEST_PACKAGE          = 'NFStest'
-NFSTEST_VERSION          = '2.1.6'
+NFSTEST_VERSION          = '2.1.7'
 NFSTEST_SUMMARY          = 'NFS Test Suite'
 NFSTEST_AUTHOR           = 'Jorge Mora'
 NFSTEST_AUTHOR_EMAIL     = 'mora@netapp.com'
